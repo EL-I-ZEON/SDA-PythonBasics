5c61b0c3d345a705ccf032eefa047dbe092b5cda
pm0702 <pm0702@att.com>
2024-09-15 11:34:11 +0200

    Fixed Posilovna.py

--- Posilovna.py
+++ Posilovna.py
@@ -4,15 +4,18 @@
 def hodnoceni_bolavosti (n):
     if n == 1:  return " tak to vážně nemyslíš vážně?"
 
-    elif n >= 2 and n <= 10: return " Budeš mít mírne nepohodlí."
+    if n >= 2 and n <= 10: return " Budeš mít mírne nepohodlí."
 
-    elif n >=11 and n <=20: return " Možná budeš mít mírně bolavé nohy."
+    if n >=11 and n <=20: return " Možná budeš mít mírně bolavé nohy."
 
-    elif n >=21 and n <=30: return "pomalinku cítíš oheň"
+    if n >=21 and n <=30: return "pomalinku cítíš oheň"
 
-    elif n >=31 and n <=40: return " Invalidita se blíží"
+    if n >=31 and n <=40: return " Invalidita se blíží"
 
-    elif n >=41 and n <=50: return  " mohl by někdo prosím zavolat sanitku?"
+    if n >=41 and n <=50: return  " mohl by někdo prosím zavolat sanitku?"
+
+    elif n >= 51:
+        return "zavolejte pohřebáky"
 
 # počet provedených dřepu
 
